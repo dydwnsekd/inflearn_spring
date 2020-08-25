4edb1f2f972b1c55d6257c6c098e0231f0e7b537
dydwnsekd <dydwnsekd123@gmail.com>
2020-08-25 21:37:16 +0900

    56페이지까지 완료 design, order 페이지 문제 있음 2장 완료시점에서 재점검 예정

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -1,9 +1,10 @@
-package tacos;
+package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -41,7 +42,16 @@ public class DesignTacoController {
         }
 
         model.addAttribute("taco", new Taco());
+
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        // 3장에서 진행
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(
--- taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -1,4 +1,4 @@
-package sia.tacocloud;
+package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
